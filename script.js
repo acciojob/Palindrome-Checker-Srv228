5c91f235cbc54bf3b058f8a721e2386d3311b550
Sourav Dey <102379824+Srv228@users.noreply.github.com>
2023-01-24 13:28:12 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 // complete the given function
 
 function palindrome(str){
+	let rev_str = "";
+    for( let i = str.length-1 ;i >= 0 ;i--)
+    {
+      rev_str+= str[i];
+    }
+    // return reverse string
+    if (str.toLowerCase()===rev_str.toLowerCase()) {
+        return true
+    } else {
+        return false
+    }
+
+
 
 }
 module.exports = palindrome
